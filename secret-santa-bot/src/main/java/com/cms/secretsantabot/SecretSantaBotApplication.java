5cbda18f5ab218143b1dbe93dd0b587a492d1447
lokesh-kakran <76099545+lokesh-kakran@users.noreply.github.com>
2020-12-16 15:23:28 +0530

    Add files via upload

---
+++ secret-santa-bot/src/main/java/com/cms/secretsantabot/SecretSantaBotApplication.java
@@ -0,0 +1,13 @@
+package com.cms.secretsantabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretSantaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretSantaBotApplication.class, args);
+	}
+
+}
---
+++ secret-santa-bot/src/test/java/com/cms/secretsantabot/SecretSantaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.secretsantabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretSantaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
