eb4b977d851e2302c5de33b42cab3b7e2a1a4382
Lokesh Kumar <lokesh.kumar@appdirect.com>
2020-12-18 00:30:20 +0530

    security same as mgmt service

--- secret-santa-bot/src/main/java/com/cms/secretsantabot/config/BotConfiguration.java
+++ secret-santa-bot/src/main/java/com/cms/secretsantabot/config/BotConfiguration.java
@@ -1,21 +1,35 @@
 package com.cms.secretsantabot.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-public class BotConfiguration {
+@EnableWebSecurity
+public class BotConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+			.cors().configurationSource(corsConfigurationSource());
+	}
 
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
-			}
-		};
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080","*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT","DELETE"));
+		configuration.setAllowedHeaders(Arrays.asList("content-type"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
+
 }
