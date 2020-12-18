dea68f60f80a3bf989fb8bd843701fb27dc6b9b8
Lokesh Kumar <lokesh.kumar@appdirect.com>
2020-12-18 16:57:42 +0530

    commiting algo changes

--- secret-santa-bot/src/main/java/com/cms/secretsantabot/servicesImpl/SecretNameGeneratingServiceImpl.java
+++ secret-santa-bot/src/main/java/com/cms/secretsantabot/servicesImpl/SecretNameGeneratingServiceImpl.java
@@ -25,9 +25,9 @@ public class SecretNameGeneratingServiceImpl implements SecretNameGeneratingServ
 
 	@Override
 	public void saveAllMatchedEmployees() {
-		List<Employee> empList = employeeRepository.findAll();
-		if (!CollectionUtils.isEmpty(empList)) {
-			generateConnections(empList).forEach((fromEmpId,toEmpId) ->{
+		List<Integer> empIdList = getAllEmpId();
+		if (!CollectionUtils.isEmpty(empIdList)) {
+			generateConnections(empIdList).forEach((fromEmpId,toEmpId) ->{
 				Employee employee = employeeRepository.getEmployeesById(fromEmpId);
 				employee.setToEmpId(toEmpId);
 				employeeRepository.save(employee); //updates the employee
@@ -38,81 +38,82 @@ public class SecretNameGeneratingServiceImpl implements SecretNameGeneratingServ
 		}
 	}
 
-	private Map<Integer, Integer> generateConnections(List<Employee> empList) {
-		log.info("Secret Name Generation : Started");
+	private Map<Integer, Integer> generateConnections(List<Integer> empList) {
+		List<Integer> numbers = new ArrayList<Integer>();
+		for(Integer j = 0; j <empList.size(); j++) {
+			numbers.add(empList.get(j));
+		}
+		List<Integer> choices = new ArrayList<Integer>();
 		Map<Integer, Integer> fromToMap = new HashMap<>();
-		if (empList.size() == 1) {
-			fromToMap.put(empList.get(0).getId(), empList.get(0).getId());
-			return fromToMap;
-		} else if (empList.size() == 2) {
-			fromToMap.put(empList.get(0).getId(), empList.get(1).getId());
-			fromToMap.put(empList.get(1).getId(), empList.get(0).getId());
-			return fromToMap;
-		} else {
-			List<Integer> allEmpId = new ArrayList<>();
-			for (int iTemp = 0; iTemp < empList.size(); iTemp++) {
-				allEmpId.add(empList.get(iTemp).getId()); //get all the employee ID, considering empId numeric
+		int choice;
+		Integer start, temp;
+		Random rand = new Random();
+		choice = rand.nextInt(numbers.size());
+		choices.add(numbers.get(choice));
+		start = numbers.get(choice);
+		numbers.remove(choice);
+		boolean selfLoop = true;
+		boolean pickStart = false;
+		while(numbers.size() > 2 || pickStart) {
+			rand = new Random();
+			choice = rand.nextInt(numbers.size());
+			choices.add(numbers.get(choice));
+			temp = numbers.get(choice);
+			numbers.remove(temp);
+			if(selfLoop && !pickStart) {
+				selfLoop = false;
+				numbers.add(start);
 			}
-			List<Integer> choices = new ArrayList<Integer>();
-			Integer choice, start, temp;
-			Random random = new Random();
-			choice = random.nextInt(allEmpId.size());
-			choices.add(allEmpId.get(choice));
-			start = allEmpId.get(choice);
-			allEmpId.remove(choice);
-			boolean selfLoop = true;
-			boolean pickStart = false;
-			while (allEmpId.size() > 2 || pickStart) {
-				random = new Random();
-				choice = random.nextInt(allEmpId.size());
-				choices.add(allEmpId.get(choice));
-				temp = allEmpId.get(choice);
-				allEmpId.remove(temp);
-				if (selfLoop && !pickStart) {
-					selfLoop = false;
-					allEmpId.add(start);
-				} else if (!selfLoop && pickStart) {
-					start = temp;
-					pickStart = false;
-					selfLoop = true;
-				} else {
-					if (temp == start) {
-						pickStart = true;
-					}
+			else if (!selfLoop && pickStart) {
+				start = temp;
+				pickStart = false;
+				selfLoop = true;
+			}
+			else {
+				if(temp == start) {
+					pickStart = true;
 				}
 			}
-			if (selfLoop && allEmpId.size() == 2) {
-				choice = random.nextInt(2);
-				choices.add(allEmpId.get(choice));
-				choices.add(allEmpId.get(1 - choice));
+		}
+		if(selfLoop && numbers.size() == 2) {
+			choice = rand.nextInt(2);
+			choices.add(numbers.get(choice));
+			choices.add(numbers.get(1-choice));
+			choices.add(start);
+		} else {
+			if(numbers.get(0) == start) {
+				choices.add(numbers.get(1));
 				choices.add(start);
 			} else {
-				if (allEmpId.get(0) == start) {
-					choices.add(allEmpId.get(1));
-					choices.add(start);
-				} else {
-					choices.add(allEmpId.get(0));
-					choices.add(start);
-				}
+				choices.add(numbers.get(0));
+				choices.add(start);
 			}
-
-			pickStart = true;
-			start = choices.get(0);
-			for (int indexOfChoice = 0; indexOfChoice < choices.size(); indexOfChoice++) {
-				if (!pickStart) {
-					//Temporary log, needs to be commented or removed
-					log.info("EmpId {}.  is gifting to EmpId  {}.", choices.get(indexOfChoice - 1), choices.get(indexOfChoice));
-					fromToMap.put(choices.get(indexOfChoice - 1), choices.get(indexOfChoice));
-					if (choices.get(indexOfChoice).equals(start)) {
-						pickStart = true;
-					}
-				} else {
-					start = choices.get(indexOfChoice);
-					pickStart = false;
-				}
-			}
-			return fromToMap;
 		}
+
+		pickStart = true;
+		start = choices.get(0);
+		for(int b = 0; b < choices.size(); b++) {
+			if(!pickStart) {
+				fromToMap.put(choices.get(b-1), choices.get(b));
+			//	log.info("EmpId {}.  is gifting to EmpId  {}.", choices.get(b-1) , choices.get(b));
+				if(choices.get(b).equals(start)) {
+					pickStart = true;
+				}
+			} else {
+				start = choices.get(b);
+				pickStart = false;
+			}
+		}
+		return fromToMap;
+	}
+
+	private List<Integer> getAllEmpId(){
+		List<Employee> emps = employeeRepository.findAll();
+		List<Integer> empIdList = new ArrayList<Integer>();
+		for (int i = 0; i <emps.size(); i++) {
+			empIdList.add(emps.get(i).getId());
+		}
+		return empIdList;
 	}
 
 }
